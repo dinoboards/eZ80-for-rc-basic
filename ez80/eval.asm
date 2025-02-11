8e7d8ec87d4815a8fed4e2b1349ff8e8cccb7a5c
Dean Netherton <dinoboards@netherton.org>
2025-02-11 16:27:25 +1100

    adl: dim local adjusted for 24bit addressing

--- ez80/eval.asm
+++ ez80/eval.asm
@@ -1,2 +1,19 @@
 	include "macros.inc"
+
+	GLOBAL	EXPR_24BIT_INT
+
+EXPR_24BIT_INT:
+	CALL	EXPRI		; RESULT IN HLH'L' C SHOULD BE ZERO
+	; U(HL) = l
+	; H = H'
+	; L = L'
+
+	EXX
+	LD	(conversion_store), HL
+	EXX
+	LD	A, L
+	LD	(conversion_store + 2), A
+	LD	HL, (conversion_store)
+	RET
+
 	include "../src/EVAL.Z80"
