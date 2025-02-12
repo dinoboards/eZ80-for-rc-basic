684c057279cf06e7ae861511b7a6290d8d22c300
Dean Netherton <dinoboards@netherton.org>
2025-02-12 12:01:31 +1100

    adl: extended the logging/tracing to also log out the current SP and its top n values

--- ez80/debug.c
+++ ez80/debug.c
@@ -102,23 +102,41 @@ void inspect_all() {
   }
 }
 
-void log_info(const char *name, uint24_t af_, uint24_t bc_, uint24_t de_,
-              uint24_t hl_, uint24_t af, uint24_t bc, uint24_t de, uint24_t hl,
-              uint24_t ix, uint24_t iy) {
+void log_info(const char *name, uint24_t *sp, uint24_t af_, uint24_t bc_,
+              uint24_t de_, uint24_t hl_, uint24_t af, uint24_t bc, uint24_t de,
+              uint24_t hl, uint24_t ix, uint24_t iy) {
   printf("log: %s.\r\nAF:%X, BC: %X, DE: %X, HL: %X, AF':%X, BC': %X, DE': %X, "
          "HL': %X, ix: %X, iy: %X\r\n",
          name, af, bc, de, hl, af_, bc_, de_, hl_, ix, iy);
 
+  printf("*sp: (%p) ", sp);
+  for (int i = 0; i < 7; i++)
+    printf("%X ", sp[i]);
+
+  printf("\r\n");
+
   uint8_t *p = (uint8_t *)ix;
   printf("*ix: ");
-  for (int i = -6; i < 20; i++)
+  for (int i = -6; i < 0; i++)
     printf("%x ", p[i]);
+
+  printf(" | ");
+
+  for (int i = 0; i < 20; i++)
+    printf("%x ", p[i]);
+
   printf("\r\n");
 
   p = (uint8_t *)iy;
   printf("*iy: ");
-  for (int i = -6; i < 20; i++)
+  for (int i = -6; i < 0; i++)
     printf("%x ", p[i]);
+
+  printf(" | ");
+
+  for (int i = 0; i < 20; i++)
+    printf("%x ", p[i]);
+
   printf("\r\n");
 
   p = (uint8_t *)hl;
