829420a3308210d4700a6084e5e7ae3f794f6806
Dean Netherton <dinoboards@netherton.org>
2025-09-15 19:53:09 +1000

    ez80: vdu driver removed and delegated to firmware

--- ez80/vdu/vdu_08.c
+++
@@ -1,6 +0,0 @@
-#include "../vdu.h"
-
-void vdu_bs() {
-  if (current_tpos.x > tviewport.left)
-    current_tpos.x--;
-}
--- ez80/vdu/vdu_18.c
+++
@@ -1,7 +0,0 @@
-#include "../vdu.h"
-
-// VDU: 18 (2 bytes)
-void vdu_gcol() {
-  current_operation_mode = data[0];
-  current_gfg_colour     = data[1];
-}
